9d3711aa852312c782f3c03be849a6d8fae036c2
nadav-galili <67638018+nadav-galili@users.noreply.github.com>
2020-09-29 20:16:48 +0300

    fixed issues

--- todos list/js/app.js
+++ todos list/js/app.js
@@ -17,12 +17,35 @@ async function getTOdos() {
 getTOdos();
 
 let newItems = "";
-let btn = document.getElementById('myBtn');
-// getting the name and post from the form
-btn.addEventListener("click", function () {
-    let name = document.getElementById('fname').value;
-    let post = document.getElementById('postBody').value;
-    newItems = `<li class="list-group-item">${name}<br>${post}</li>`;
+const endPoint = "https://jsonplaceholder.typicode.com/todos";
+
+$('#myBtn').on('click', () => {
+    if ($('#fname').val() == "") {
+        alert("Task must be filled out");
+        return false;
+    }
+    const newPost = {
+        userId: 1,
+        title: $('#fname').val(),
+        body: $("input[type='radio']:checked").val(),
+    };
+    console.log($('#fname'));
+
+    const endpoint = "https://jsonplaceholder.typicode.com/posts";
+    fetch(endpoint, {
+            method: "POST",
+            body: JSON.stringify(newPost),
+        })
+        .then((response) => response.json())
+        .then((post) => {
+            console.log(post);
+        });
+    console.log(newPost);
+
+    let name = $('#fname').val();
+    let completed = $("input[type='radio']:checked").val()
+    newItems = `<li class="list-group-item">${name}<br>completed:${completed}</li>`;
     console.log(newItems);
     $('#myList').prepend(newItems)
-});
+
+})
